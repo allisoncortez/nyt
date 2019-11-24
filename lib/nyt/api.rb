f35798b6c34b71731c2c32c00f6e9a1d61191b70
allison cortez <allizetorc@gmail.com>
2019-11-24 04:38:32 +0000

    api updates

--- lib/nyt/api.rb
+++ lib/nyt/api.rb
@@ -1,6 +1,10 @@
 class Nyt::API 
   def fetch 
     key = "C9IM2TSLGbkXfHjIpP2ThdAEHTkIZzFV"
-    url = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=yourkey"
+    url = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=#{key}"
+    response = HTTParty.get(url)
+    response["results"].each do |book|
+      
+    binding.pry
   end 
 end
