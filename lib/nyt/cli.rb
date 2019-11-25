3425e504eac467b48ee5af252b13ee115a5e59cd
allison cortez <allizetorc@gmail.com>
2019-11-25 01:13:25 +0000

    adding logic to CLI

--- lib/nyt/cli.rb
+++ lib/nyt/cli.rb
@@ -6,4 +6,30 @@ class Nyt::CLI
     puts list.title
     end
   end 
+  
+  def best_sellers
+    #puts Best Sellers list from the category user has specified..
+    # Category Name here:
+    # 1. Book Title by Author(example of list item..)
+  end
+  
+  def menu 
+    while input != "See yah!"
+      puts "For more info, input a number from the list.If you'd like to leave, input 'See yah!'"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "1. Title by Author - description of book - amazon link"
+      when "2"
+        puts "2. Title by Author - description of book - amazon link"
+      when "3"
+        puts "3. Title by Author - description of book - amazon link"
+      end 
+    end
+  end
+  
+  def bye 
+    puts "goodbye message here"
+  end
+  
 end
