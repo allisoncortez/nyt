9c76d15aa771eaab88c3e28d1707c9653b92faa2
allison cortez <allizetorc@gmail.com>
2019-11-27 21:33:55 +0000

    iterating through top 5 lists

--- lib/nyt/api.rb
+++ lib/nyt/api.rb
@@ -1,26 +1,26 @@
 class Nyt::API 
   def fetch 
     key = "C9IM2TSLGbkXfHjIpP2ThdAEHTkIZzFV"
-    # url = "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=#{key}"
+    
+    #top 5 current bestsellers of each genre
     url = "https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=#{key}"
   
     response = HTTParty.get(url)
     # title = response["results"]["books"][0]["title"]
     
-    response["results"]["lists"][0]["books"].each do |book|
-      # binding.pry
-      
-      # rank = book["rank"]
-      # list_name = response["results"]["list_name"]
-      title = book["title"]
-      author = book["contributor"]
-      description = book["description"]
-      link = book["amazon_product_url"]
-      # buy_links = book ["buy_links"]
-      
+    response["results"]["lists"].each do |book|
+      binding.pry
+    
+      # # list_name = response["results"]["list_name"]
+      # title = book["title"]
+      # author = book["contributor"]
+      # description = book["description"]
+      # link = book["amazon_product_url"]
+      # # buy_links = book ["buy_links"]
       
       
-      Nyt::Books.new(title,author,description,link)
+   
+      # Nyt::Books.new(title,author,description,link)
     
     end
   end 
