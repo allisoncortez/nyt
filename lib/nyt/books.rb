779aa15f7f95524544835ce2430a89272d575d9c
allison cortez <allizetorc@gmail.com>
2019-11-30 01:38:33 +0000

    refactoring cli code

--- lib/nyt/books.rb
+++ lib/nyt/books.rb
@@ -1,15 +1,13 @@
 class Nyt::Books
-  attr_accessor :title, :author, :description, :links, :publisher, :isbn
+  attr_accessor :title, :author, :description, :reviews
   @@all = []
   
   
-  def initialize(title,author,description,publisher,isbn)
+  def initialize(title,author,description,reviews)
     @title = title 
     @author = author
     @description = description
-    @links = links
-    @publisher = publisher
-    @isbn = isbn
+    @reviews = reviews
     @@all << self 
   end 
   
--- lib/nyt/cli.rb
+++ lib/nyt/cli.rb
@@ -9,14 +9,15 @@ class Nyt::CLI
     
     Nyt::API.new.fetch
       Nyt::Books.all.each.with_index(1) do |book, i|
+        # binding.pry
       puts "#{i}. #{book.title} #{book.author}"
     end
+    
+    # menu
+    
   end
     
     
-#     # puts "Type a number from the book list to view it's full details."
-#     # puts "Type 'best sellers' to view the best sellers list again."
-#     # puts "To exit, type 'exit'"
   
 #   def genre_list
 #     input = gets.strip.downcase
@@ -37,74 +38,48 @@ class Nyt::CLI
 #   end
   
   
-#   # def book_info
-    
-#   # #   #get user input
-#   # #   input = gets.strip.downcase
-     
-#   # #   #spit out correct list of 5 bestsellers for the genre that was selected.
-    
-#   # #   if input.to_i > 0 
-#   # #   Nyt::API.new.fetch
-#   # #   Nyt::Books.all.each.with_index(1) do |list, i|
-#   # #     # binding.pry
-#   # #   puts "#{i}. #{list.title} #{list.author}"
-#   # #   puts "Description: #{list.description}"
-#   # #   puts "Publisher: #{list.publisher} - ISBN number: #{list.isbn}"
-
-    
-#   # # #     profile_page = Nokogiri::HTML(open(profile_url))
-#   # # #   student_hash = {}
-#   # # #   profile_page.css(".social-icon-container a").each do |profile|
-#   # # #   link = profile.attributes["href"].value 
-#   # # #   if link.include?("twitter")
-#   # # #     student_hash[:twitter] = link
-#   # # #     elsif link.include?("linkedin")
-#   # # #       student_hash[:linkedin] = link
-#   # # #     elsif link.include?("github")
-#   # # #       student_hash[:github] = link
-#   # # #     else 
-#   # # #       student_hash[:blog] = link 
-#   # # #     end
-#   # # #   end 
-#   # # #   student_hash
-#   # # # end
+  def book_info
+    input = gets.strip.downcase
+ 
+    if input.to_i > 0 
+    Nyt::API.new.fetch
+    Nyt::Books.all.each.with_index(1) do |list, i|
+      # binding.pry
+    puts "#{i}. #{list.title} #{list.author}"
+    puts "Description: #{list.description}"
+    puts "Publisher: #{list.publisher} - ISBN number: #{list.isbn}"
     
     
-    
-#   # #     end
-#   # #   end
-#   # end
+    end
+  end
+  end
   
-#   # def menu 
-#   #   # input = nil
-#   #   # while input != "exit"
-#   #   #   puts "MAIN MENU:"
-#   #   #   puts "1. Input a number from the best seller list to view it's details."
-#   #   #   puts "2. Type 'best_sellers' to view the top 5 best sellers again."
-#   #   #   puts "3. Type 'genre list' to view the full list of genres."
-#   #   #   # puts "4. Type a number from the genre list to view it's bestsellers."
-#   #   #   puts "5. Type 'exit' to leave."
+  def menu 
+    input = nil
+    while input != "exit"
+      puts "MAIN MENU:"
+      puts "1. Input a number from the best seller list to view it's details."
+      puts "2. Type 'best sellers list' to view the full list again."
+      puts "5. Type 'exit' to leave."
       
-#   #   #   input = gets.strip.downcase
+      input = gets.strip.downcase
 
 
-#   #   #   if input.to_i > 0 
-#   #   #     Nyt::API.new.fetch 
-#   #   #     Nyt::Books.all.each.with_index(1) do |list|
-#   #   #       # binding.pry
-#   #   #     puts "#{list.title} #{list.author} - #{list.links}"
-#   #   #     end
-#   #   #     elsif input == "list"
-#   #   #     best_sellers 
-#   #   #   else 
-#   #   #     puts "INVALID INPUT: type 'list' or 'exit'"
-#   #   #   end 
-#   #   # end
-#   # end
+      if input.to_i > 0 
+        Nyt::API.new.fetch 
+        Nyt::Books.all.each.with_index(1) do |list|
+          # binding.pry
+        puts "#{list.title} #{list.author} - #{list.links}"
+        end
+        elsif input == "list"
+        best_sellers 
+      else 
+        puts "INVALID INPUT: type 'list' or 'exit'"
+      end 
+    end
+  end
   
-#   # def bye 
-#   #   puts "Bye Nerd."
-#   # end
-# end
+  def bye 
+    puts "Bye Nerd."
+  end
 end
