2e4b17fad21aa136301916171eed387d3670ee81
allison cortez <allizetorc@gmail.com>
2019-12-02 02:59:15 +0000

    changed run speeds and fixed menu method

--- lib/nyt/cli.rb
+++ lib/nyt/cli.rb
@@ -2,82 +2,80 @@ class Nyt::CLI
   
   def call
     puts "\nHey there, Bookworm!"
-    puts "Would you like to see top 15 bestseller list?"
-    puts "type Y/N"
-    
+    sleep(1)
+    puts "Would you like to see top 15 bestseller list? (Y/N)\n"
+    sleep(0.25)
     input = gets.strip.downcase
+    
+    
     if input == "y"
     best_seller_list
     elsif input == "n"
-    menu
+    bye
+    elsif 
+    puts "invalid input: type 'list' or 'exit'."
     end
     
-    # menu
-    get_chosen_book
   end
-  
-  # def get_list
-  #   # @books = Nyt::Books.all
-  #   @books = Nyt::Books.all
-  # end
+ 
   
   def best_seller_list
-    puts "\nChoose a book to see it's details."
-    sleep(1)
+    puts "\nHere are Penguin's Top 20 Bestsellers."
+    sleep(0.25)
+    
     Nyt::API.new.fetch
     Nyt::Books.all.uniq.each.with_index(1) do |book, i|
       puts "#{i}. #{book.title} by #{book.author}"
     end
-  end
-  
-  
-  def get_chosen_book
-    chosen_book = gets.strip.to_i
-    book_details(chosen_book)
     sleep(1)
     menu
   end
   
-  def book_details(chosen_book)
-    book = Nyt::Books.all[chosen_book - 1]
+  
+  # def get_chosen_book
+  #   chosen_book = gets.strip.to_i
+  #   book_details(chosen_book)
+  #   sleep(1)
+  #   menu
+  # end
+  
+  def book_details(input)
+    book = Nyt::Books.all[input.to_i - 1]
 
-        puts "\n--------Book Details------------------------------------------------------"
+        puts "\n--------Book Details------------------------------------------------------\n"
         puts "\n#{book.title} by #{book.author}\n"
         puts "\nDescription: #{book.description}"
         
         #only put review if there is one.. need to code this..
-        puts "\nReview: #{book.reviews}"
+        puts "Review: #{book.reviews}\n"
   end
+  
   
   def menu 
     input = nil
+    
     while input != "exit"
-      puts "MAIN MENU:"
-      puts "1. Input a number from the best seller list to view it's details."
-      # puts "2. Type 'list' to view the full list again."
-      puts "5. Type 'exit' to leave."
-      
+      puts "\nInput a number from the best seller list to view it's details.\n"
+      puts "Type 'list' to view the full list again."
+      puts "If you'd like to exit, type 'exit'.\n"
       input = gets.strip.downcase
-      if input.to_i > 0 
-      book_details
       
-        elsif input == "list"
-        get_chosen_book
-        
-        elsif input == "exit"
+      
+      if input.to_i > 0 
+        book_details(input)
+      elsif input == "list"
+        best_seller_list
+      elsif input == "exit"
         bye
       else 
-        puts "INVALID INPUT: type 'list' or 'exit'"
-        sleep(1)
-        menu
+        puts "\nINVALID INPUT: type 'list' or 'exit'\n"
       end 
     end
   end
   
+  
   def bye 
-    puts "Bye Nerd."
+    puts "\nBye Nerd.\n"
   end
   
-  
-  
-  end
+end
