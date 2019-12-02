5a1436db38414e801f8a5aecea7c68b69069cb2c
allison cortez <allizetorc@gmail.com>
2019-12-02 03:56:49 +0000

    planning logic for links method to grab all buy_links

--- lib/nyt/api.rb
+++ lib/nyt/api.rb
@@ -2,19 +2,22 @@ class Nyt::API
   
   def fetch
     key = "C9IM2TSLGbkXfHjIpP2ThdAEHTkIZzFV"
-    # url = "https://api.nytimes.com/svc/books/v3/lists/best-sellers/history.json?offset=20&publisher=Penguin&api-key=#{key}"
-    
     url = "https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction.json?api-key=#{key}"
-    
     
     response = HTTParty.get(url)
     response["results"]["books"].each do |book|
-      # binding.pry
       title = book["title"]
       author = book["author"]
       publisher = book["publisher"]
       description = book["description"]
-      link = book["amazon_product_url"]
+      
+      
+      link = book["buy_links"][0]["url"]
+      #this would be my amazonlink
+      
+      # apple_link = book["buy_links"][1]["url"]
+      # barnes_n_noble_link = book["buy_links"][2]["url"]
+      # local_bookseller_link = book["buy_links"][3]["url"]
       
     
       Nyt::Books.new(title,author,publisher,description,link)
