88477f5074670b7ec2b119c8e196ac7123ab66f1
allison cortez <allizetorc@gmail.com>
2019-12-05 03:06:17 +0000

    deleting old comments

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,11 +1,11 @@
 require 'pry'
 require 'httparty'
+require 'colorize'
 
 require_relative 'nyt/version'
 require_relative 'nyt/api'
 require_relative 'nyt/cli'
 require_relative 'nyt/books'
-# require_relative 'nyt/genres'
 
 
 
